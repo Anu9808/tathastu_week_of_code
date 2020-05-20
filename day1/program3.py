0f503eb8296fca377146d094fb66b11c64250ed6
Anu9808 <47288256+Anu9808@users.noreply.github.com>
2020-05-20 14:19:22 +0530

    Create program3.py

---
+++ day1/program3.py
@@ -0,0 +1,6 @@
+num1= int(input("Enter first number "))
+num2 = int(input("Enter second number "))
+
+#swapping without third variable
+num1, num2 = num2, num1
+print("Numbers after swapping: {} {}".format(num1,num2))
