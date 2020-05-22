d3767507f611930cf93611cc88d57e4b0190c080
Anu9808 <47288256+Anu9808@users.noreply.github.com>
2020-05-22 14:04:06 +0530

    Create program1.py

---
+++ day4/program1.py
@@ -0,0 +1,7 @@
+
+tup = tuple(map(int, input("Enter the space separated values ").strip().split()))
+num = int(input("Enter the element whose occurence frequency u want to know  "))
+print(tup.count(num))
+
+  
+  
