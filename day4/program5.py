657f80fc1ad497983bf9f5d3c8c19534f2779651
Anu9808 <47288256+Anu9808@users.noreply.github.com>
2020-05-22 15:46:28 +0530

    Update program5.py

--- day4/program5.py
+++ day4/program5.py
@@ -1,14 +1,13 @@
 def main():
-  votes_no = int(input("Enter the no of votes: "))
-  vote_list = {}
-  for i in range(votes_no):
-      name = input("Enter the name of the Candidate ")
-      if name not in vote_list:
-          vote_list[name] = 1
-      else:
-          vote_list[name] += 1
-  name_li = sorted(vote_list,key = lambda k:k[1],reverse = True)
-  name_li.sort()
-  print("The name of the candidate who won the election is", name_li[0])
-  
-main()
+    votes_no = int(input("Enter the no of votes: "))
+    vote_list = []
+    for i in range(votes_no):
+        vote_list.append(input("Enter the name of the Candidate to cast the Vote: "))
+    vote = []
+    for name in set(vote_list):
+        vote.append((name, vote_list.count(name)))  
+    vote.sort(key = lambda x : x[0] ) # sorting name lexico.
+    vote.sort(key = lambda x : x[1],reverse = True) #by no. of votes
+    print("The name of the candidate who won the election is", vote[0][0]) #winner
+main() 
+
