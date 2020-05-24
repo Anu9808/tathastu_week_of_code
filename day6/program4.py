45a32f7accd71093d0aae41c5e86b30aa1242c84
Anu9808 <47288256+Anu9808@users.noreply.github.com>
2020-05-24 21:50:03 +0530

    Create program4.py

---
+++ day6/program4.py
@@ -0,0 +1,29 @@
+def next_int(s):
+
+    if s==sorted(s,reverse=True):
+        return 'present number is Largest'
+
+    if s == sorted(s):
+        s[len(s)-2], s[len(s)-1] = s[len(s)-1], s[len(s)-2]
+        str_num = [str(i) for i in s]
+        return int("".join(str_num))
+
+        
+    for i in range(len(s)-1,0,-1):
+        if s[i]>s[i-1]:
+            index=i-1
+            break
+    
+    smallest_index = s.index(min(filter(lambda i: i> s[index],s[index+1:])))
+    x=s.pop(smallest_index)
+
+    s.insert(index,x)
+    s[index+1:] =sorted(s[index+1:]) 
+    str_num = [str(i) for i in s]
+    return int("".join(str_num))
+def main():
+
+  num = list(map(int,input("Enter number:")))
+  print("\nThe next possible greatest number is:",next_greater(num))
+  
+main()
